0281658c3523d1ccbb6cc3e15890cbe204cf1dd5
biasia <luziye@luziyedeMacBook-Pro.local>
2022-11-02 17:41:59 +0800

    back to try again

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: process.env.TIDB_HOST,
-      port: process.env.TIDB_PORT,
+      host: env('DATABASE_HOST', 'gateway01.us-west-2.prod.aws.tidbcloud.com'),
+      port: env.int('DATABASE_PORT', 4000),
       database: env('DATABASE_NAME', 'strapi'),
-      user: process.env.TIDB_USER,
-      password: process.env.TIDB_PASSWORD,
+      user: env('DATABASE_USERNAME', '4KVKyWFT3zeQC6e.root'),
+      password: env('DATABASE_PASSWORD', 'bot2Onr8gwGaS91T'),
       // ssl: {
       //   minVersion: 'TLSv1.2',
       //   rejectUnauthorized: true
