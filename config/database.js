966c32e3692b78e57f33e3951d059ff9d395560c
biasia <luziye@luziyedeMacBook-Pro.local>
2022-11-02 17:24:48 +0800

    adjust to vercel integration

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', 'gateway01.us-west-2.prod.aws.tidbcloud.com'),
-      port: env.int('DATABASE_PORT', 4000),
+      host: process.env.TIDB_HOST,
+      port: process.env.TIDB_PORT,
       database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', '4KVKyWFT3zeQC6e.root'),
-      password: env('DATABASE_PASSWORD', 'bot2Onr8gwGaS91T'),
+      user: process.env.TIDB_USER,
+      password: process.env.TIDB_PASSWORD,
       // ssl: {
       //   minVersion: 'TLSv1.2',
       //   rejectUnauthorized: true
