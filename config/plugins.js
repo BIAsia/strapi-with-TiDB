087c28d5c354783698d366e94068170d5de3691e
biasia <luziye@luziyedeMacBook-Pro.local>
2022-11-03 13:14:22 +0800

    add plugin config

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+// path: ./config/plugins.js
+
+module.exports = () => ({
+    graphql: {
+      enabled: true,
+      config: {
+        playgroundAlways: true,
+        defaultLimit: 10,
+        maxLimit: 20,
+        apolloServer: {
+          tracing: true,
+        },
+      }
+    }
+  })
