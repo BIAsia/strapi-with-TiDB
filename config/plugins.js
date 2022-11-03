98729c79d16f98fd1c420166a5668f1aaad5ed4e
biasia <luziye@luziyedeMacBook-Pro.local>
2022-11-03 15:20:04 +0800

    adjust plugin config

--- config/plugins.js
+++ config/plugins.js
@@ -8,7 +8,7 @@ module.exports = {
     config: {
       endpoint: '/graphql',
       shadowCRUD: true,
-      playgroundAlways: false,
+      playgroundAlways: true,
       depthLimit: 7,
       amountLimit: 100,
       apolloServer: {
